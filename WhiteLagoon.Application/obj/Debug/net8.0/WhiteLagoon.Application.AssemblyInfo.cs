47fbc10e4bc180fdc7897492d9db46e552990f62
Youssef-Remah <yremah8@gmail.com>
2025-02-20 19:48:20 +0200

    Add Villa Model to Domain.Entities

--- WhiteLagoon.Application/obj/Debug/net8.0/WhiteLagoon.Application.AssemblyInfo.cs
+++ WhiteLagoon.Application/obj/Debug/net8.0/WhiteLagoon.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhiteLagoon.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da52060a367f32099316f02950c82c9d32d58a55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7222ecc106441676bd1bf386ba89a483addb112")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhiteLagoon.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhiteLagoon.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
