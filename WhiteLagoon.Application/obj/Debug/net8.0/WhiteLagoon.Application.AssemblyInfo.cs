815ba98a74312580af52664fcef447cb0297145e
Youssef-Remah <yremah8@gmail.com>
2025-02-20 20:00:30 +0200

    Add ApplicationDbContext to Infrastructure.Data

--- WhiteLagoon.Application/obj/Debug/net8.0/WhiteLagoon.Application.AssemblyInfo.cs
+++ WhiteLagoon.Application/obj/Debug/net8.0/WhiteLagoon.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhiteLagoon.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7222ecc106441676bd1bf386ba89a483addb112")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47fbc10e4bc180fdc7897492d9db46e552990f62")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhiteLagoon.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhiteLagoon.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
