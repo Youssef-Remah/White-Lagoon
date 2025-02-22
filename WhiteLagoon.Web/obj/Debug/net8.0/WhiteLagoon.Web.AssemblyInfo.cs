c0728e9c2854f1deea0e17262269a575ed7162a7
Youssef-Remah <yremah8@gmail.com>
2025-02-22 11:52:39 +0200

    chore: change app theme to dark

--- WhiteLagoon.Web/obj/Debug/net8.0/WhiteLagoon.Web.AssemblyInfo.cs
+++ WhiteLagoon.Web/obj/Debug/net8.0/WhiteLagoon.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhiteLagoon.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a72056baee67e9deefb7fec1c26f5b019967664d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48871092fe9c8125411be969e9b7366d404f24f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhiteLagoon.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhiteLagoon.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
