2152d526cc1d2095e53c5f93becf093c485a1411
Youssef-Remah <yremah8@gmail.com>
2025-02-23 11:57:47 +0200

    feat: add validation attributes for Villa model
    - Add validation attributes for Villa model properties.
    - Enable server-side validation for villa creation.

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
 namespace WhiteLagoon.Web.Controllers
@@ -12,15 +14,32 @@ namespace WhiteLagoon.Web.Controllers
             _dbContext = dbContext;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-             var villas = _dbContext.Villas.ToList();
+             var villas = await _dbContext.Villas.ToListAsync();
 
             return View(villas);
         }
 
+        [HttpGet]
         public IActionResult Create()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Villa newVilla)
+        {
+            if (ModelState.IsValid)
+            {
+                await _dbContext.Villas.AddAsync(newVilla);
+
+                await _dbContext.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
             return View();
         }
     }
