26a0303e58ca1dff05165897eedb3e20fad7ee68
Youssef-Remah <yremah8@gmail.com>
2025-03-05 00:33:33 +0200

    feat: add VillaNumberController and Index view
    - Create VillaNumberController
    - Add Index action to display all VillaNumbers
    - Create Index view to show the list of VillaNumbers

---
+++ WhiteLagoon.Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public VillaNumberController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+             var villaNumbers = await _dbContext.VillaNumbers.ToListAsync();
+
+            return View(villaNumbers);
+        }
+    }
+}
