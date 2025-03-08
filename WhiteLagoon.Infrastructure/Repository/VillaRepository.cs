a9a98b21ff9015c281685966c258f101896fdeaf
Youssef-Remah <yremah8@gmail.com>
2025-03-09 00:53:10 +0200

    feat: add VillaRepository with CUD operations and register in DI container
    - Implement VillaRepository as a concrete class for IVillaRepository
    - Add methods for Create, Update, and Delete operations
    - Register IVillaRepository in the DI container

---
+++ WhiteLagoon.Infrastructure/Repository/VillaRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq.Expressions;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Infrastructure.Repository
+{
+    public class VillaRepository : IVillaRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public VillaRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddNewVilla(Villa newVilla)
+        {
+            await _dbContext.Villas.AddAsync(newVilla);
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteVilla(Villa villa)
+        {
+            _dbContext.Villas.Remove(villa);
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public Task<IEnumerable<Villa>> GetAllVillas(Expression<Func<Villa, bool>>? filter = null, string? includeNavigationProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Villa>> GetSingleVilla(Expression<Func<Villa, bool>> filter, string? includeNavigationProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateVilla(Villa villa)
+        {
+            _dbContext.Villas.Update(villa);
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
--- WhiteLagoon.Web/Program.cs
+++ WhiteLagoon.Web/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Application.Common.Interfaces;
 using WhiteLagoon.Infrastructure.Data;
+using WhiteLagoon.Infrastructure.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IVillaRepository, VillaRepository>();
 
 var app = builder.Build();
 
