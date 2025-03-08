870260abd934daa548a0611803e8d2e5edc2939e
Youssef-Remah <yremah8@gmail.com>
2025-03-09 00:26:00 +0200

    chore: add IVillaRepository interface in the Application layer
    - Create IVillaRepository interface in ~/.Application.Common.Interfaces

---
+++ WhiteLagoon.Application/Common/Interfaces/IVillaRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Application.Common.Interfaces
+{
+    public interface IVillaRepository
+    {
+        public Task<IEnumerable<Villa>> GetAllVillas(Expression<Func<Villa, bool>>? filter = null, string? includeNavigationProperties = null);
+
+        public Task<IEnumerable<Villa>> GetSingleVilla(Expression<Func<Villa, bool>> filter, string? includeNavigationProperties = null);
+
+        public Task AddNewVilla(Villa newVilla);
+
+        public Task UpdateVilla(Villa villa);
+
+        public Task DeleteVilla(Villa villa);
+    }
+}
