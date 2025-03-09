1f5b41ddb8065ce9d5935a866ef4c71314fcd0b8
Youssef-Remah <yremah8@gmail.com>
2025-03-09 04:07:06 +0200

    feat: implement UnitOfWork class to manage repositories
    - Create a concrete UnitOfWork class that implements IUnitOfWork
    - Initialize IVillaRepository with a concrete implementation

---
+++ WhiteLagoon.Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Infrastructure.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IVillaRepository Villa { get; private set; }
+
+        private readonly ApplicationDbContext _dbContext;
+
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+            Villa = new VillaRepository(_dbContext);
+        }
+    }
+}
