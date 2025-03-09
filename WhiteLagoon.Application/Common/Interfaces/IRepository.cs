1ac63f5410295458056c3032a1a86ea18c942603
Youssef-Remah <yremah8@gmail.com>
2025-03-09 02:56:16 +0200

    chore: add generic repository interface for common CRD operations
    - Create a generic interface to standardize common Create, Read, and Delete operations for all entities

---
+++ WhiteLagoon.Application/Common/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace WhiteLagoon.Application.Common.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        public Task<IEnumerable<T>> GetAllEntities(Expression<Func<T, bool>>? filter = null, string? includeNavigationProperties = null);
+
+        public Task<T?> GetSingleEntity(Expression<Func<T, bool>> filter, string? includeNavigationProperties = null);
+
+        public Task AddNewEntity(T newEntity);
+
+        public Task DeleteEntity(T entity);
+    }
+}
