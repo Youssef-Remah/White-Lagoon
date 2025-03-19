c8e2a1374cf1e5a63673c7937b2f37776c8f7a9a
Youssef-Remah <yremah8@gmail.com>
2025-03-20 00:56:32 +0200

    feat: add LoginViewModel and RegisterViewModel
    - Created LoginViewModel for handling user login
    - Created RegisterViewModel for handling user registration

---
+++ WhiteLagoon.Web/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhiteLagoon.Web.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+
+        [Display(Name = "Phone Number")]
+        public string? PhoneNumber { get; set; }
+
+
+        [Required]
+        public string Email { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Password does not match")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+
+        public string? RedirectUrl { get; set; }
+    }
+}
