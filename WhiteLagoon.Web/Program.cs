4d848956d75d70d511a34dd20ff4ceeae2b12728
Youssef-Remah <yremah8@gmail.com>
2025-05-05 15:28:45 +0300

    chore: register Syncfusion library and add license key for PDF and Word exports
    - Enable document export functionality by registering Syncfusion services and setting the required license key

--- WhiteLagoon.Web/Program.cs
+++ WhiteLagoon.Web/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Stripe;
+using Syncfusion.Licensing;
 using WhiteLagoon.Application.Common.Interfaces;
 using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
@@ -26,6 +27,10 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
 
 StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
+
+SyncfusionLicenseProvider.RegisterLicense(builder.Configuration
+                                                 .GetSection("Syncfusion:Licensekey")
+                                                 .Get<string>());
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
